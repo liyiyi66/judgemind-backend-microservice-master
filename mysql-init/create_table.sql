833fe2dd993c7ad686016e67c19b5bbc5614ce22
怡怡 <3058730574@qq.com>
2025-11-16 21:21:07 +0800

    JudgeMind智能判题系统

--- mysql-init/create_table.sql
+++ mysql-init/create_table.sql
@@ -1,12 +1,11 @@
 # 数据库初始化
-# @author <a href="https://github.com/liyupi">程序员鱼皮</a>
-# @from <a href="https://yupi.icu">编程导航知识星球</a>
+
 
 -- 创建库
-create database if not exists yuoj;
+create database if not exists judgemind;
 
 -- 切换库
-use yuoj;
+use judgemind;
 
 -- 用户表
 create table if not exists user
